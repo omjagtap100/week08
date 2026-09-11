12f997f4ece4eab9336b8498e3ac3c81946221e7
Om Jagtap <om.jagtap@tribox.me>
2026-09-11 22:42:19 +1000

    Deploying SIT722 8.1P pipeline

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -141,3 +141,4 @@ def health_check() -> dict[str, str]:
     }# Triggering SIT722 8.1P CI/CD Pipeline
 # Triggering SIT722 8.1P CI/CD Pipeline
 # Triggering SIT722 8.1P CI/CD Pipeline
+# Triggering SIT722 8.1P CI/CD Pipeline
