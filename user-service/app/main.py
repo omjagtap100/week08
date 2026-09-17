78fc2c8d70b69cc3c6eb063d889f49a91bbe5b44
Om Jagtap <om.jagtap@tribox.me>
2026-09-17 16:27:42 +1000

    Week 09 monitoring deployment

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -143,3 +143,4 @@ def health_check() -> dict[str, str]:
 # Triggering SIT722 8.1P CI/CD Pipeline
 # Triggering SIT722 8.1P CI/CD Pipeline
 # Triggering SIT722 8.1P CI/CD Pipeline
+# Week 09 monitoring run
